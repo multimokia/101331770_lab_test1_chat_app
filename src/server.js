e6578550b8fab3f9471772f048e2cd50c05f8e7a
multimokia <multimokia3@gmail.com>
2023-02-05 23:01:21 -0500

    basic websockets

--- src/server.js
+++ src/server.js
@@ -8,9 +8,10 @@ config();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "";
 
-// Init our express server and
+// Init our express server and socketio server
 const app = express();
 const server = new Server(app);
+const io = SocketIOServer(server);
 
 // Add public dir to host html and create its route
 app.use(express.static("public"));
@@ -23,3 +24,38 @@ server.listen(
     PORT,
     () => console.log(`Server listening on port ${PORT}`)
 );
+
+const rooms = {};
+
+// Now prep our socket connection
+io.on("connection", (socke) => {
+    socket.on("user_join", (user, room) => {
+        console.log(`${user.username} has joined ${room}`);
+
+        // If room isn't stored, we add it
+        if (rooms[room] === undefined) {
+            rooms[room] = { users: {} }
+        }
+
+        // And add the user to the room by id
+        rooms[room].users[user.id] = user;
+
+        // Finally, officially do this on socket.io
+        socket.join(room);
+
+        // And broadcast that the user has joined
+        socket.to(room).emit("server_broadcast_user_join", user);
+    });
+
+    socket.on("user_sends_message", (message) => {
+        socket.to(message.room).emit(message);
+    });
+
+    socket.on("user_leave", (user, room) => {
+        console.log(`${user.username} has left ${room}`);
+
+        // Remove user from room
+        delete rooms[room].users[user.id];
+        socket.leave(room)
+    });
+});
