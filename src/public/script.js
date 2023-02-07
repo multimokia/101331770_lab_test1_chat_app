8bb560020d008223443ae3ddc9bb05f7a1e0775d
multimokia <multimokia3@gmail.com>
2023-02-07 13:23:54 -0500

    fix a cors issue

--- src/public/script.js
+++ src/public/script.js
@@ -106,7 +106,7 @@ function outputUsers(users) {
 }
 
 //Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
+document.getElementById('leave-btn').addEventListener('click', function() {
     const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
     if (leaveRoom) {
         socket.emit("user_leave", USER, ROOM);
